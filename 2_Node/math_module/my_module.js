c311ae10b9a56f642266a5c722b4c85858bcab4e
nramiscal <nramiscal@gmail.com>
2018-04-05 10:43:03 -0400

    added my_module

---
+++ 2_Node/math_module/my_module.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    greet : function(name){
+        console.log(`Hello ${name}!`);
+    },
+
+    add: function(num1,num2){
+        let sum = num1 + num2;
+        console.log(`${num1} + ${num2} = ${sum}`);
+    }
+
+}
